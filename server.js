4002b0e6ab48105a1d9f9c52820951fe6efd12f0
Baiakhmet <baiakhmet.zhekeyev@gmail.com>
2024-05-08 19:42:31 +0500

    sprint_1: fix server

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import express from "express";
-import { resolve, join } from "path";
-
-const __dirname = resolve();
+import { fileURLToPath } from "url";
+import path from "path";
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(join(__dirname, "dist")));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static("./dist"));
+
+app.use("/*", (_, res) =>
+  res.sendFile(path.join(__dirname, "dist/index.html")),
+);
 
 app.listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
