33990a8f94c7b91dd90ec550d891afb8069fd79d
Baiakhmet <baiakhmet.zhekeyev@gmail.com>
2024-05-08 20:40:58 +0500

    sprint_1: fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
-  plugins: [],
-  root: "./src",
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "src/components"),
+    }),
+  ],
+  root: resolve(__dirname, "src"),
+  build: {
+    outDir: resolve(__dirname, "src"),
+  },
   server: {
     port: 3000,
-  },
-  build: {
-    sourcemap: false,
-    outDir: "../dist",
   },
 });
