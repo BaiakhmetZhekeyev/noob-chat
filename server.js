6ba74b825631132a822103a7180211a6014a428f
Baiakhmet <baiakhmet.zhekeyev@gmail.com>
2024-05-08 17:02:02 +0500

    sprint_1: configure the server

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 import express from "express";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
+import { resolve, join } from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = resolve();
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(resolve(__dirname, "dist")));
-
-app.get("/", (req, res) => {
-  res.sendFile(resolve(__dirname, "dist", "index.html"));
-});
+app.use(express.static(join(__dirname, "dist")));
 
 app.listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
