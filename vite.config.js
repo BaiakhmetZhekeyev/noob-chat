8273117512294d21fc1337f5cc765cbaee71398a
Baiakhmet <baiakhmet.zhekeyev@gmail.com>
2024-05-08 16:48:12 +0500

    sprint_1: change vite port to 3000

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [],
   root: "./src",
+  server: {
+    port: 3000,
+  },
   build: {
     sourcemap: false,
     outDir: "../dist",
