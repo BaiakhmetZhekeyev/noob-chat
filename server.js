bed759e1a011659de3b3d89e5582a63e5b850e99
b.zhekeyev <baiakhmet.zhekeyev@gmail.com>
2024-06-01 01:59:13 +0500

    sprint_2: (fixTest) fix eslint errors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from "express";
-import { fileURLToPath } from "url";
-import path from "path";
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 const app = express();
 const port = 3000;
@@ -8,11 +8,9 @@ const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static("./dist"));
+app.use(express.static('./dist'));
 
-app.use("/*", (_, res) =>
-  res.sendFile(path.join(__dirname, "dist/index.html")),
-);
+app.use('/*', (_, res) => res.sendFile(path.join(__dirname, 'dist/index.html')));
 
 app.listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
